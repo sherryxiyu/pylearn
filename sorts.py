7385a5b978b9e2045ae78848969ae465d586e5ce
SherryFu <sdrongxin@qq.com>
2023-09-11 01:04:39 +0800

    commit0911

--- sorts.py
+++ sorts.py
@@ -1,8 +1,8 @@
 def bubble_sort(arr):
     n = len(arr)
-    for i in range(1, n):
-        for j in range(i, n-1):
-            if arr[j]>arr[j+1]:
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 def quick_sort(arr, left, right):
